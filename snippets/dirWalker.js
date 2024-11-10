58d8c9408e2deab56af27e0f9c7fa544f014b1ab
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-10 08:32:25 +0000

    snippets

---
+++ snippets/dirWalker.js
@@ -0,0 +1,67 @@
+var fs = require("fs");
+var path = require("path");
+var verifyFile = require('./verifyFile')
+
+function classifyFileType(filePath) {
+    const ext = path.extname(filePath).toLowerCase();
+
+    const audioExtensions = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.wma'];
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.tiff'];
+    const documentExtensions = ['.pdf', '.doc', '.docx', '.ppt', '.pptx', '.txt', '.xls', '.xlsx'];
+    const codeExtensions = ['.js', '.html', '.css', '.py', '.java', '.c', '.cpp', '.rb', '.go', '.php'];
+
+    if (audioExtensions.includes(ext)) {
+        return 'audio';
+    } else if (imageExtensions.includes(ext)) {
+        return 'image';
+    } else if (documentExtensions.includes(ext)) {
+        return 'document';
+    } else if (codeExtensions.includes(ext)) {
+        return 'code';
+    } else {
+        return 'unknown';
+    }
+}
+
+function readDir(basedir, dir) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(dir);
+                files = files.map(file => {
+                    const filePath = path.join(dir, file);
+                    const stats = fs.statSync(filePath);
+                    let type = '';
+                    if (stats.isDirectory()) {
+                        type = 'directory';
+                    } else {
+                        type = 'file';
+                    }
+					if (type == 'file') {
+						var openable = verifyFile.checkEditableFile(file);
+						var kind = classifyFileType(file);
+						
+					}
+                    return {
+                        name: file,
+                        filePath: path.relative(basedir, filePath).replace(file, ''),
+                        cleanPath: path.relative(basedir, filePath),
+                        isDirectory: stats.isDirectory(),
+                        size: stats.size,
+                        createdAt: stats.birthtime,
+                        modifiedAt: stats.mtime,
+                        id: stats.birthtime.getTime(),
+                        type,
+                     	openable,
+                     	kind,
+                     };
+                    i++;
+                });
+                resolve(files);
+            }
+        });
+    });
+}
+module.exports = readDir;
---
+++ snippets/verifyFile.js
@@ -0,0 +1,45 @@
+var VALID_EXTENSIONS = [
+    'html', 'htm', 'txt', 'text', 'css', 'js', 'jpg', 'jpeg', 'png', 'apng', 'gif', 'svg', 'md', 'markdown', 'eot', 'ttf', 'woff', 'woff2', 'json', 'geojson', 'csv', 'tsv', 'mf', 'ico', 'pdf', 'asc', 'key', 'pgp', 'xml', 'mid', 'midi', 'manifest', 'otf', 'webapp', 'less', 'sass', 'rss', 'kml', 'dae', 'obj', 'mtl', 'scss', 'webp', 'avif', 'xcf', 'epub', 'gltf', 'bin', 'webmanifest', 'knowl', 'atom', 'opml', 'rdf', 'map', 'gpg', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts', 'glb', 'py', 'php'
+];
+
+var VALID_EDITABLE_EXTENSIONS = [
+    'html', 'htm', 'txt', 'js', 'css', 'scss', 'md', 'markdown', 'manifest', 'less', 'webmanifest', 'xml', 'json', 'opml', 'rdf', 'svg', 'gpg', 'pgp', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts', 'php',
+];
+
+function checkFileName(name) {
+    name.toLowerCase();
+    name.replace(/\s/g, '');
+    for (let i = 0; i < VALID_EXTENSIONS.length; i++) {
+        if (name.endsWith(VALID_EXTENSIONS[i])) {
+            console.log('Valid extension found:', VALID_EXTENSIONS[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkCreatableFile(name) {
+    name.toLowerCase();
+    name.replace(/\s/g, '');
+    for (let i = 0; i < VALID_EDITABLE_EXTENSIONS.length; i++) {
+        if (name.endsWith(VALID_EDITABLE_EXTENSIONS[i])) {
+            console.log('Valid extension found:', VALID_EDITABLE_EXTENSIONS[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkEditableFile(name) {
+    name.toLowerCase();
+    name.replace(/\s/g, '');
+    for (let i = 0; i < VALID_EDITABLE_EXTENSIONS.length; i++) {
+        if (name.endsWith(VALID_EDITABLE_EXTENSIONS[i])) {
+            console.log('Valid extension found:', VALID_EDITABLE_EXTENSIONS[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = {checkCreatableFile, checkEditableFile, checkFileName};
