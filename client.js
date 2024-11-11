96146f3721c53bc39b9fbc4fc596359ed655c73c
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-11 09:20:20 +0000

    add directory parameter to dashboard route

--- client.js
+++ client.js
@@ -155,6 +155,7 @@ app.get("/dashboard", async (req, res) => {
       res.render("dashboard", {
         title: "Dashboard",
         files: JSON.parse(data),
+        dir: req.query.dir || "",
       });
     })
     .catch((error) => {
