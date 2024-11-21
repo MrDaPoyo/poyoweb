96286c1c1a8598d1981a6640f96fb44f2058c649
MrDaPoyo <wiiuforever25@gmail.com>
2024-11-21 09:58:27 +0000

    login form

--- admin.js
+++ admin.js
@@ -42,8 +42,64 @@ const checkAuthMiddleware = (req, res, next) => {
   });
 };
 
+const notLoggedInMiddleware = (req, res, next) => {
+  if (res.locals.loggedIn) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+
+const loggedInMiddleware = (req, res, next) => {
+  if (!res.locals.loggedIn) {
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
+}
+
 router.get("/", checkAuthMiddleware,	async (req, res) => {
 	res.render("adminIndex", {title: "Index"});	
+});
+
+router.get("/auth/login", notLoggedInMiddleware, (req, res) => {
+  res.render("adminLogin", { title: "Login" });
+});
+
+router.post("/auth/login", notLoggedInMiddleware, async (req, res) => {
+  const { user, password } = req.body;
+  if (!user || !password) {
+    res.status(400).json({ error: "Missing required fields", success: false });
+    return;
+  } else {
+    try {
+      const response = await fetch(process.env.API_URL + "admin/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: user,
+          password: password,
+        }),
+        timeout: 5000,
+      });
+      const text = await response.text();
+      const data = text ? JSON.parse(text) : {};
+      if (data.success) {
+        res.cookie("auth", data.jwt, { httpOnly: true });
+        res.locals.loggedIn = true;
+        res.redirect("/?message=Successfully logged in");
+      } else {
+        res.clearCookie("auth");
+        res.locals.loggedIn = false;
+        res.render("/?message=Error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "An error occurred; " + error });
+    }
+  }
 });
 
 router.use((req, res, next) => {
